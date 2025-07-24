bb1615e17abf4899d686f40e7bdda42765c0d164
Jeff Mader <jeff.mader@gmail.com>
2025-07-24 06:18:55 -1000

    Shouldn't have + in datetime sent to DB

--- get_dimm_data.py
+++ get_dimm_data.py
@@ -133,7 +133,7 @@ def get_dimm_data(utDate='', mdir='.', log_writer=''):
 
     if log_writer:
         log_writer.info('get_dimm_data.py complete for {}'.format(utDate))
-    wxdb.updateWxDb(dbDate, 'cfht_seeing', datetime.utcnow().strftime('%Y%m%d+%H:%M:%S'), log_writer)
+    wxdb.updateWxDb(dbDate, 'cfht_seeing', datetime.utcnow().strftime('%Y%m%d %H:%M:%S'), log_writer)
 
     for n in ['mass', 'dimm', 'masspro']:
         joinSeq = (mdir, '/', utDate, '.mkwc.', n, '.dat')
